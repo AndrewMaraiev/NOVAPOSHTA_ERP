e9a0d80178cff88563a7a00105689530ff38102c
popenko-ikrok <popenko.d@ikrok.net>
2023-12-07 10:42:00 +0000

    add(Shipment Providers, Payment Methods): Added new Doctypes, deleted Shipping Providers

--- erpnext_shipping/erpnext_shipping/doctype/payment_methods/payment_methods.js
+++ erpnext_shipping/erpnext_shipping/doctype/payment_methods/payment_methods.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2023, Frappe and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Shipping Providers', {
+frappe.ui.form.on('Payment Methods', {
 	// refresh: function(frm) {
 
 	// }
--- erpnext_shipping/erpnext_shipping/doctype/shipment_providers/test_shipment_providers.py
+++ erpnext_shipping/erpnext_shipping/doctype/shipment_providers/test_shipment_providers.py
@@ -5,5 +5,5 @@
 from frappe.tests.utils import FrappeTestCase
 
 
-class TestShippingProviders(FrappeTestCase):
+class TestShipmentProviders(FrappeTestCase):
 	pass
